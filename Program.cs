991f304f2839ac359e0a83d82f7d885ae8f2fe1f
habibmirsyad <habmirs@gmail.com>
2022-03-26 12:42:14 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace KPL_1302201138_MOD5_JURNAL_GGR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("anu");
+        }
+    }
